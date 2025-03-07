429cfd80f7ab9342885e387c4d47709e83bedc27
devanshsinghmadvit <devansh@madvit.com>
2025-03-08 01:24:31 +0530

    delete pnp file

--- app/(chat)/api/vote/route.ts
+++ app/(chat)/api/vote/route.ts
@@ -1,6 +1,5 @@
 
 import { getChatById, getVotesByChatId, voteMessage } from '@/lib/db/queries';
-import { useSession } from 'next-auth/react';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -10,7 +9,7 @@ export async function GET(request: Request) {
     return new Response('chatId is required', { status: 400 });
   }
 
-  const session =useSession()
+  const session = {user:{id:"assad",exipres:"33342442424342424",email:"test@yopmail.com"}};
 
 
   if (!session || !session.user || !session.user.email) {
@@ -44,7 +43,7 @@ export async function PATCH(request: Request) {
     return new Response('messageId and type are required', { status: 400 });
   }
 
-  const session =useSession()
+  const session = {user:{id:"assad",exipres:"33342442424342424",email:"test@yopmail.com"}};
 
 
   if (!session || !session.user || !session.user.email) {
